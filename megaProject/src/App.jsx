a04ff3794d3555244cf479d1e98f462225ab8d3f
Thelaxman <gusainlakshman@gmail.com>
2024-09-04 14:23:06 -0600

    components added

--- megaProject/src/App.jsx
+++ megaProject/src/App.jsx
@@ -1,14 +1,34 @@
-
-
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 function App() {
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
 
-  return (
-    <>
- 
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
