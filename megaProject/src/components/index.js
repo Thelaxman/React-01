9c2253869a7578a846859d2c6e0e1eb64b823f4e
Thelaxman <gusainlakshman@gmail.com>
2024-09-04 14:42:45 -0600

    header section initialised

--- megaProject/src/components/index.js
+++ megaProject/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/header";
 import Footer from "./footer/footer.jsx";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn.jsx";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
