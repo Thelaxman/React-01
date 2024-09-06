db92be73cbb33774ee16206a645bf9b09b040da9
Thelaxman <gusainlakshman@gmail.com>
2024-09-05 23:31:05 -0600

    navigation tab added

--- megaProject/src/components/header/Header.jsx
+++ megaProject/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import LogoutBtn from "./LogoutBtn";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
@@ -33,7 +34,38 @@ function Header() {
     },
   ];
 
-  return <div></div>;
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
